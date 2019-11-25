aee2fdf0e1694248c6a08bc22e2af6aab59297f9
ilhuitem0c <jc11111001001@gmail.com>
2019-11-24 20:53:49 -0600

    Simplifica el parametro de ciudades

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Location = ({city}) => {
-    //Destructuring
-    return(
-        <div><h1>{city}</h1></div>
+const Location = ({city}) => (
+        <div>
+            <h1>
+                {city}
+            </h1>
+        </div>
     );
-};
 
 export default Location; 
