cc38627d94c46330b847192c2ca67f3b456662a9
ilhuitem0c <jc11111001001@gmail.com>
2019-12-08 20:32:51 -0600

    Mapeando las ciudades y creano un compnente por cada una

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -2,18 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import WeatherLocation from './WeatherLocation';
 
-const LocationList = ({ cities }) => {
-    console.log(cities); 
-    
-    return(
+const strToComponents = cities => (
+    cities.map( city => <WeatherLocation city={city} /> )
+);
+
+const LocationList = ({ cities }) => (
         <div>
-            <WeatherLocation city="Buenos Aires, ar" />
-            <WeatherLocation city="Bogota, col" />
-            <WeatherLocation city="Washington, us" />
-            <WeatherLocation city="Barcelona, es" />
+            {
+                strToComponents(cities)
+            }
         </div>
-    );
-}; 
+);
 
 LocationList.propTypes = {
     cities: PropTypes.array.isRequired,
