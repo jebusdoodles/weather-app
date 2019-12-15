143e70817fced64eac3e249cfdccc871336a2414
ilhuitem0c <jc11111001001@gmail.com>
2019-12-14 21:00:59 -0600

    Everything to the normal

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
           <AppBar position='sticky'>
             <Toolbar>
               <Typography variant='title' color='inherit'>
-                The Weather App
+                Weather App
               </Typography>
             </Toolbar>
           </AppBar>
