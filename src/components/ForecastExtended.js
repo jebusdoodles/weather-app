06d0f6a5399a691e3c7fde69654ee544ab8ba690
ilhuitem0c <jc11111001001@gmail.com>
2019-12-27 15:05:19 -0600

    add new component to check weather for day

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ForecastItem from './ForecastItem';
+import './styles.css';
+
+const days= [
+    'Lunes', 
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+];
 
 class ForecastExtended extends Component {
+    renderForecastItemDays(){
+        return days.map( day => (<ForecastItem weekDay={day} />));
+    }
+
     render(){
         const { city } = this.props;
 
         return(
-            <div>Pronostico extendido para {city}</div>
+            <div>
+                <h2 className='forecast-title'>Pronostico extendido para {city}</h2>
+                {this.renderForecastItemDays()}
+            </div>
         );
     }
 }
