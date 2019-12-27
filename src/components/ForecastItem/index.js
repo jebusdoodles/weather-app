07631d781f60ed150a60cdf644f9c7cc3eda9163
ilhuitem0c <jc11111001001@gmail.com>
2019-12-27 15:20:09 -0600

    Add data to Weather for day

--- src/components/ForecastItem/index.js
+++ src/components/ForecastItem/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types'; 
+import WeatherData from './../WeatherLocation/WeatherData';
 
-const ForecastItem = ({ weekDay }) => (
-    <div>{weekDay}</div>
+const ForecastItem = ({ weekDay, hour, data  }) => (
+    <div>
+        <div>{weekDay} Hora: {hour}</div>
+        <WeatherData data={data} />
+    </div>
 );
 
 ForecastItem.propTypes = {
     weekDay: PropTypes.string.isRequired, 
+    hour: PropTypes.number.isRequired,
 }
 
 export default ForecastItem;
