59efd6b9f897c670e966d8f049c7c61a323096b9
ilhuitem0c <jc11111001001@gmail.com>
2019-12-31 12:30:42 -0600

    Change from ComponentDidMount to ComponentWillReceiveProps

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -35,7 +35,19 @@ class ForecastExtended extends Component {
     }
 
     componentDidMount(){
-        const url_forecast = `${url_base_weather}?q=${this.props.city}&appid=${api_key}`;
+        // Es importante ejecutarlo la primera vez 
+        this.updateCity(this.props.city); 
+    }
+
+    componentWillReceiveProps(nextProps){
+        if(nextProps.city !== this.props.city){
+            this.setState({ forecastData:null });
+            this.updateCity(nextProps.city);
+        }
+    }
+
+    updateCity = city => {
+        const url_forecast = `${url_base_weather}?q=${city}&appid=${api_key}`;
 
         fetch(url_forecast).then(
             data => (data.json())
