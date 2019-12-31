298e5287e13cff01b3fbec8cc23b63a2ef8c2b1b
ilhuitem0c <jc11111001001@gmail.com>
2019-12-30 21:03:26 -0600

    Small changes on css

--- src/components/ForecastItem/index.js
+++ src/components/ForecastItem/index.js
@@ -4,7 +4,7 @@ import WeatherData from './../WeatherLocation/WeatherData';
 
 const ForecastItem = ({ weekDay, hour, data  }) => (
     <div>
-        <div>{weekDay} Hora: {hour}</div>
+        <h2>{weekDay} - {hour}</h2>
         <WeatherData data={data} />
     </div>
 );
