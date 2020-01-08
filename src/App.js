9c74f9768d636d8a95fa61ee746100128cf30850
ilhuitem0c <jc11111001001@gmail.com>
2020-01-08 15:21:04 -0600

    Refactorizacion para simplificar App.js y encerrar el Connect de Redux en un contenedor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
 import './App.css';
 import { Grid, Col, Row } from 'react-flexbox-grid';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar'; 
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar'; 
-import LocationList from './components/LocationList';
+import LocationListContainer from './containers/LocationListContainer';
 import ForecastExtended from './components/ForecastExtended'; 
-import { setCity } from './actions';
 
 const cities = [
   'Buenos Aires, ar', 
@@ -27,13 +25,6 @@ class App extends Component {
     this.state = { city: null };
   }
 
-  handleSelectedLocation = city => {
-    this.setState({ city });
-    console.log(`handleSelectedLocation ${city}`);
-
-    this.props.setCity(city); 
-  }
-
   render(){
     const { city } = this.state;
     return (
@@ -49,10 +40,8 @@ class App extends Component {
         </Row>
         <Row>
           <Col xs={12} md={6}>
-              <LocationList 
+              <LocationListContainer 
                       cities={cities} 
-                      onSelectedLocation={this.handleSelectedLocation}
-                
               />
           </Col>
           <Col xs={12} md={6}>
@@ -72,12 +61,4 @@ class App extends Component {
 
 }
 
-const mapDispatchToProps = dispatch => (
-  {
-    setCity: value => dispatch(setCity(value))
-  }
-);
-
-const AppConnected = connect(null, mapDispatchToProps)(App);
-
-export default AppConnected; 
+export default App; 
---
+++ src/containers/LocationListContainer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setCity } from './../actions';
+import LocationList from './../components/LocationList'; 
+
+class LocationListContainer extends Component{
+
+    handleSelectedLocation = city => {
+        this.props.setCity(city); 
+      }
+
+    render(){
+        return (
+            <LocationList 
+                cities={this.props.cities} 
+                onSelectedLocation={this.handleSelectedLocation}
+            />  
+        );
+    }
+}
+
+const mapDispatchToProps = dispatch => (
+    {
+      setCity: value => dispatch(setCity(value))
+    }
+);
+
+export default connect(null, mapDispatchToProps)(LocationListContainer); 
