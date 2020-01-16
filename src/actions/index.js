6a0691d594c29ea2791255d00a1102e615fa2dd9
ilhuitem0c <jc11111001001@gmail.com>
2020-01-15 21:45:54 -0600

    Se agrega el midware redux thunk para mejorar el manejo de estados

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,27 @@
 export const SET_CITY ='SET_CITY';
 
 export const setCity = (payload)=> ({ type: SET_CITY, payload});
+
+export const api_key="0c1e529959fc5a55f0a36ba5a01d1e81"; 
+export const url_base_weather = "http://api.openweathermap.org/data/2.5/forecast";
+
+export const fecthForecast = payload => {
+    return dispatch => {
+        const url_forecast = `${url_base_weather}?q=${city}&appid=${api_key}`;
+
+        // activar en el estaod un indicador de busqueda
+
+        fetch(url_forecast).then(
+            data => (data.json())
+        ).then(
+            weather_data => {
+                const forecastData = transformForecast(weather_data);
+                console.log(forecastData); 
+
+                //modificar el estado con el resultado de la promise
+
+            }
+        );
+        return;
+    }
+};
