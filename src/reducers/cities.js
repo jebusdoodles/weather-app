597e73c06afb4a53e2c47d4ae68dd8e97593b166
ilhuitem0c <jc11111001001@gmail.com>
2020-01-20 22:15:55 -0600

    creacion de un nuevo reducer cities para administrar el forecastdata pasado

---
+++ src/reducers/cities.js
@@ -0,0 +1,14 @@
+import { SET_FORECAST_DATA } from './../actions'; 
+
+export const cities = (state = {}, action) => {
+    switch(action.type){
+        case SET_FORECAST_DATA:
+            const { city, forecastData } = action.payload;
+            //en caso de recibir informacion la almacena en un reducer 
+            //emacscript 6 
+            return { ...state, [city]: {forecastData}}; 
+        default:
+            // en otro caso retorna el estado
+            return state; 
+    }
+}
