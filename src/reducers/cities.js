71c055b1f1ec3b4d6d535f1f063f07bb0ca4a6cf
ilhuitem0c <jc11111001001@gmail.com>
2020-01-30 15:16:31 -0600

    Se agrega la biblioteca de reselect para el almacenamiento de datos

--- src/reducers/cities.js
+++ src/reducers/cities.js
@@ -1,3 +1,4 @@
+import { createSelector } from 'reselect';  
 import { SET_FORECAST_DATA } from './../actions'; 
 
 export const cities = (state = {}, action) => {
@@ -14,4 +15,4 @@ export const cities = (state = {}, action) => {
 }
 
 // Diccionario y sobre este diccionario esta ForecastData
-export const getForecastDataFromCities = (state, city) => state[city] && state[city].forecastData; 
+export const getForecastDataFromCities = createSelector((state, city) => state[city] && state[city].forecastData, forecastData => forecastData);
