bd99be3dd73e8de775506ea03aa0e70d346a55c5
ilhuitem0c <jc11111001001@gmail.com>
2020-02-09 21:47:01 -0600

    Cambio minimo

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,26 +4,6 @@ import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
 
-/* 
-
-    componentDidMount() {
-        this.handleUpdateClick();
-    }
-    
-    handleUpdateClick = () => {
-        const api_weather = getUrlWeatherByCity(this.state.city);
-        fetch(api_weather).then( resolve => {
-            return resolve.json();
-        }).then(data => {
-            const newWeather = transformWeather(data); 
-            this.setState({
-                data: newWeather
-            });
-        });
-    }
-
-
-*/
 const WeatherLocation = ({ onWeatherLocationClick, city, data }) => (
     <div className='weatherLocationCont' onClick={onWeatherLocationClick} >
                 <Location city={city} />
