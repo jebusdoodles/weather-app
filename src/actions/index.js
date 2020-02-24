36ac48766cb0a36c8d022c46616f592b50e91766
ilhuitem0c <jc11111001001@gmail.com>
2020-02-23 22:34:56 -0600

    oculto claves de acceso

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,9 +14,6 @@ const setWeatherCity = payload=>({type: SET_WEATHER_CITY, payload});
 const setCity = (payload)=> ({ type: SET_CITY, payload});
 const setForecastData = payload =>({type: SET_FORECAST_DATA, payload })
 
-export const api_key="0c1e529959fc5a55f0a36ba5a01d1e81"; 
-export const url_base_weather = "http://api.openweathermap.org/data/2.5/forecast";
-export const url_weather = "http://api.openweathermap.org/data/2.5/weather";
 
 // Accion que trae el pronostico extendido
 export const setSelectedCity = payload => {
