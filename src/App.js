83aea946b7d019fad6be19b9753631811ce61a20
ilhuitem0c <jc11111001001@gmail.com>
2020-11-04 17:04:40 -0600

    Refactorizacion a containers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 /* eslint-disable no-console */
 import React, { Component } from 'react';
 
-import { connect } from 'react-redux'; 
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Paper from 'material-ui/Paper';
 import AppBar from 'material-ui/AppBar';
-import LocationList from './components/LocationList';
+import LocationListContainer from './containers/LocationListContainer';
 import ForecastExtended from './components/ForecastExtended';
 
-import { setCity } from './actions'; 
 
 import './App.css';
 
@@ -29,12 +26,6 @@ class App extends Component {
     this.state = { city: null };
   }
 
-  handleSelectedLocation = city => {
-    this.setState({ city });
-    console.log(`handleSelectedLocation ${city}`);
-
-    this.props.setCity(city); 
-  }
   
   render() {
     const { city } = this.state;
@@ -49,8 +40,9 @@ class App extends Component {
           </Row>
           <Row>
             <Col xs={12} md={6}>
-              <LocationList cities={cities} 
-              onSelectedLocation={this.handleSelectedLocation} ></LocationList>
+              <LocationListContainer 
+                    cities={cities} 
+              ></LocationListContainer>
             </Col>
             <Col xs={12} md={6}>
               <Paper zDepth={4}>
@@ -70,12 +62,5 @@ class App extends Component {
   }
 }
 
-
-const mapDispatchToPropsActions = dispatch => ({
-  setCity: value => dispatch(setCity(value))
-}); 
-
-const AppConnected = connect(null, mapDispatchToPropsActions)(App);
-
-export default AppConnected; 
+export default App;
 
